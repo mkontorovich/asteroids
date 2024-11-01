51d24e2847987160bf37f2301988e75cb4992810
Mikhail Kontorovich <mikhail.kontorovich@gmail.com>
2024-10-31 20:33:44 -0500

    implemented high scores

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import *
@@ -8,20 +5,20 @@ from asteroid import *
 from asteroidfield import *
 from shot import *
 import sys
+from scoreboard import *
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-
     # Load the Asteroids image
-    icon = pygame.image.load('asteroids_logo.png')
+    icon = pygame.image.load('assets/asteroids_logo.png')
 
     pygame.init()
     pygame.display.set_icon(icon)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    bg_color = (0, 0, 0)
+    bg_color = constants.SCREEN_BG_COLOR
     
     clock = pygame.time.Clock()
     dt = 0
@@ -39,8 +36,9 @@ def main():
     AsteroidField.containers = (updatable)
     Shot.containers = (shots, updatable, drawable)
 
-    player = Player(x, y)
-    asteroid_field = AsteroidField()
+    player = Player(x, y) # player is in the center
+    asteroid_field = AsteroidField() # draws the asteroid field, so we can see asteroids
+    scoreboard = ScoreBoard() # draws the scoreboard, so we can see our score
 
     game_running = True
     while (game_running):
@@ -57,15 +55,21 @@ def main():
                 if asteroid.collision_check(shot):
                     asteroid.split()
                     shot.kill()
+                    scoreboard.increase_score(1)
 
             if player.collision_check(asteroid):
                 print("Game Over!")
+                scoreboard.save_score()
+                scoreboard.reset_score()
                 sys.exit()
 
         screen.fill(bg_color)
         
         for item in drawable:
             item.draw(screen)
+
+        scoreboard.draw_high_score(screen)
+        scoreboard.draw_current_score(screen)
 
         pygame.display.flip()
 
---
+++ scoreboard.py
@@ -0,0 +1,47 @@
+import pygame
+import constants
+import json
+
+class ScoreBoard():
+    def __init__(self, 
+                 font_path=constants.SCOREBOARD_FONT_FILE,
+                 font_size=constants.SCOREBOARD_FONT_SIZE, 
+                 color=constants.SCOREBOARD_COLOR, 
+                 high_score_position=constants.SCOREBOARD_HIGH_SCORE_POSITION,
+                 current_score_position=constants.SCOREBOARD_CURRENT_SCORE_POSITION,
+                 score_file=constants.SCOREBOARD_FILE):
+        self.score = 0
+        self.color = color
+        self.high_score_position = high_score_position
+        self.current_score_position = current_score_position
+        self.font = pygame.font.Font(font_path, font_size)
+        self.score_file = score_file
+        self.high_scores = self.load_high_score()
+
+    def increase_score(self, points):
+        self.score += points
+
+    def reset_score(self):
+        self.score = 0
+
+    def draw_high_score(self, screen):
+        score_text = self.font.render(f"High Score: {self.high_scores}", True, self.color)
+        screen.blit(score_text, self.high_score_position)
+
+    def draw_current_score(self, screen):
+        score_text = self.font.render(f"Score: {self.score}", True, self.color)
+        screen.blit(score_text, self.current_score_position)
+
+    def save_score(self):
+        if self.score > self.high_scores:
+            self.high_scores = self.score
+            with open(self.score_file, 'w') as file:
+                json.dump({"high_score": self.high_scores}, file)
+
+    def load_high_score(self):
+        try:
+            with open(self.score_file, 'r') as file:
+                data = json.load(file)
+                return data.get("high_score", 0)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return 0
